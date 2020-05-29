bb3670ce70d29e57ff5b99b64c5b7bac837b2976
Santos <santoscastillojimenez@hotmail.com>
2020-05-29 03:01:41 +0200

    Commit 21

--- detallepelicula.php
+++ detallepelicula.php
@@ -36,7 +36,7 @@ if (!isset($_GET['id']) || $_GET['id'] == null) {
 
 		$pelicula = mysqli_fetch_assoc($resultpel);
 
-		if ($_SESSION['admin'] == 0 && $pelicula['cat_pel'] = 0) {
+		if ($pelicula['cat_pel'] = 0) {
 
 				echo "<script type='text/javascript'>
 				window.location='index.php';
